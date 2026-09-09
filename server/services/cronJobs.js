45665fca2a7c5ada4af514a941df9066f60716b8
ronit45 <banron@gmail.com>
2026-09-09 21:41:29 +0530

    update cron job

--- server/services/cronJobs.js
+++ server/services/cronJobs.js
@@ -5,7 +5,7 @@ import { sendTelegramNotification } from './telegramBot.js';
 
 export const initCronJobs = () => {
   // Check every 30 minutes
-  cron.schedule('*/10 * * * *', async () => {
+  cron.schedule('*/2 * * * *', async () => {
     try {
       console.log('⏰ Running Telegram notification cron job...');
       const now = new Date();
